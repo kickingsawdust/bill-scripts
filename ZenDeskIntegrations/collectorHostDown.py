5c64d4207601d2195a8ba2c8236bf261d2f8ceb5
root <root@bill6cc.zenoss.loc>
2024-09-13 10:52:45 -0400

    Some clean up and adding ZenDeskIntegrations work

---
+++ ZenDeskIntegrations/collectorHostDown.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+##############################################################################
+# Copyright (C) Zenoss, Inc. 2024, all rights reserved.
+# Author: Bill Loss 'closs  @ zenoss.com'
+##############################################################################
+
+import sys
+import zenApiLib
+import argparse
+import requests
+import json
+import re
+from requests.auth import HTTPBasicAuth
+
+parser = argparse.ArgumentParser(description='grab host and zendesk user.')
+parser.add_argument("-c", "--collector", action="store", dest="collector", required=True, help="Down Collector host ID")
+#  Username - example "admin"
+parser.add_argument("-e", "--email", action="store", dest="email", required=True, help="ZenDesk user's email to be notified")
+parser.add_argument("-E", "--evid", action="store", dest="evid", required=True, help="should be the current evid from evt/evid")
+args = parser.parse_args()
+print(args)
+
+url = "https://zenoss1724961392.zendesk.com/api/v2/tickets"
+
+payload = {
+  "ticket": {
+    "submitter_id": 29771010991501,
+    "requester": args.email,
+    "comment": {
+      "body": "Collector host " + args.collector + " is no longer connected to Zenoss Cloud, please restart, yada yada."
+    },
+    "priority": "urgent",
+    "type": "incident",
+    "subject": "Collector host " + args.collector + " offline",
+  }
+}
+
+
+headers = {
+        "Content-Type": "application/json", 
+        "Idempotency-Key": args.evid
+}
+
+
+email_address = 'closs@zenoss.com'
+api_token = 'DeqYUrk7BCXBOE7V7Pehcw70oI5v37i58ZcDnoNh'
+# Use basic authentication
+#auth = HTTPBasicAuth(f'{email_address}/token', api_token)
+auth = HTTPBasicAuth('{}/token'.format(email_address), api_token)
+
+response = requests.request(
+        "POST",
+        url,
+        auth=auth,
+        headers=headers,
+        json=payload
+)
+
+print(response.text)
+
+decoded = response.json()
+ticket_number = decoded['audit']['ticket_id']
+print("Ticket Number: %s" % str(ticket_number))
+
+# ToDo: url is hardcoded here but should probably be extracted from payload
+url = "https://zenoss1724961392.zendesk.com/agent/tickets/" + str(ticket_number)
+print("Ticket URL: %s" % url)
+
+############################################ SET HOST TO MAINT HAPPENS IN SEPARATE SCRIPT########################
+
+### Update event to store url and ticket_num as zenoss.IncidentManagement.url and zenoss.IncidentManagement.number
+
+def updateEvent(**data):
+#  import pdb; pdb.set_trace()
+  zep = zenApiLib.zenConnector(routerName = "EventsRouter")
+  response = zep.callMethod("updateDetails", **data)
+  if response.get('result', {}).get('success', False) is False:
+      raise Exception('API call returned unsucessful result.\n%s' % response)
+  return response['result']
+
+data =  {
+            "evid" : args.evid,
+            "zenoss.IncidentManagement.number" : str(ticket_number),
+            "zenoss.IncidentManagement.url" : url
+            }
+        
+api_response = updateEvent(**data)
+print(data)
+print(api_response)
+
---
+++ ZenDeskIntegrations/collectorHostUp.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+##############################################################################
+# Copyright (C) Zenoss, Inc. 2024, all rights reserved.
+# Author: Bill Loss 'closs  @ zenoss.com'
+##############################################################################
+
+import sys
+import zenApiLib
+import argparse
+import requests
+import json
+import re
+from requests.auth import HTTPBasicAuth
+
+parser = argparse.ArgumentParser(description='grab host and zendesk user.')
+parser.add_argument("-c", "--collector", action="store", dest="collector", required=True, help="Down Collector host ID")
+#  Username - example "admin"
+parser.add_argument("-e", "--email", action="store", dest="email", required=True, help="ZenDesk user's email to be notified")
+parser.add_argument("-E", "--evid", action="store", dest="evid", required=True, help="should be the current evid from evt/evid")
+parser.add_argument("-n", "--number", action="store", dest="ticketNumber", required=True, help="should be the incident from evt/zenoss.IncidentManagement.number")
+args = parser.parse_args()
+print(args)
+
+requestUrl = "https://zenoss1724961392.zendesk.com/api/v2/tickets/" + str(args.ticketNumber)
+
+print requestUrl
+
+payload = {
+  "ticket": {
+    "comment": {
+      "body": "Collector host " + args.collector + " has come back online, closing this ticket proactively. Thanks!"
+    },
+    "status": "solved",
+  }
+}
+
+
+headers = {
+        "Content-Type": "application/json",
+}
+
+
+email_address = 'closs@zenoss.com'
+api_token = 'DeqYUrk7BCXBOE7V7Pehcw70oI5v37i58ZcDnoNh'
+# Use basic authentication
+#auth = HTTPBasicAuth(f'{email_address}/token', api_token)
+auth = HTTPBasicAuth('{}/token'.format(email_address), api_token)
+
+response = requests.request(
+        "PUT",
+        requestUrl,
+        auth=auth,
+        headers=headers,
+        json=payload
+)
+
+print(response.text)
+
+decoded = response.json()
+ticket_number = decoded['audit']['ticket_id']
+print("Ticket Number: %s" % str(ticket_number))
+
+# ToDo: url is hardcoded here but should probably be extracted from payload
+url = "https://zenoss1724961392.zendesk.com/agent/tickets/" + str(ticket_number)
+print("Ticket URL: %s" % url)
+
+############################################ SET HOST TO MAINT HAPPENS IN SEPARATE SCRIPT########################
+
+### Update event to store url and ticket_num as zenoss.IncidentManagement.url and zenoss.IncidentManagement.number
+"""
+def updateEvent(**data):
+#  import pdb; pdb.set_trace()
+  zep = zenApiLib.zenConnector(routerName = "EventsRouter")
+  response = zep.callMethod("updateDetails", **data)
+  if response.get('result', {}).get('success', False) is False:
+      raise Exception('API call returned unsucessful result.\n%s' % response)
+  return response['result']
+
+data =  {
+            "evid" : args.evid,
+            "zenoss.IncidentManagement.number" : str(ticket_number),
+            "zenoss.IncidentManagement.url" : url
+            }
+        
+api_response = updateEvent(**data)
+print(data)
+print(api_response)
+"""
+
