5f824d845813a3119c94748b1a1cd3c7e1b913d2
root <root@bill6cc.zenoss.loc>
2024-09-19 14:09:12 -0400

    adding support for CC emails

--- ZenDeskIntegrations/collectorHostDown.py
+++ ZenDeskIntegrations/collectorHostDown.py
@@ -13,14 +13,31 @@ import json
 import re
 from requests.auth import HTTPBasicAuth
 
+'''
+if not ('logging' in dir()):
+    import logging
+    logging.basicConfig(
+        format = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+    )
+    logging.getLogger().setLevel(logging.ERROR)
+'''
+
 parser = argparse.ArgumentParser(description='grab host and zendesk user.')
 parser.add_argument("-c", "--collector", action="store", dest="collector", required=True, help="Down Collector host ID")
 parser.add_argument("-e", "--email", action="store", dest="email", required=True, help="ZenDesk user's email to be notified")
+parser.add_argument("-C", "--emailCCs", action="store", dest="emailCCs", required=False, nargs='+', help="List of ZenDesk user's emails to be CC'd on the ticket")
 parser.add_argument("-E", "--evid", action="store", dest="evid", required=True, help="should be the current evid from evt/evid")
 args = parser.parse_args()
 
 # ToDo: convert to proper logging
-print(args)
+#print "Args = %s " % args
+
+ccs = []
+for e in args.emailCCs:
+    ccs.append({"user_email": e, "action": "put"})
+
+# ToDo: convert to proper logging
+print "CCs = %s" % ccs
 
 url = "https://zenoss1724961392.zendesk.com/api/v2/tickets"
 
@@ -34,6 +51,7 @@ payload = {
     "priority": "urgent",
     "type": "incident",
     "subject": "Collector host " + args.collector + " offline",
+    "email_ccs": ccs
   }
 }
 
@@ -43,12 +61,13 @@ headers = {
         "Idempotency-Key": args.evid
 }
 
-# change me: should be zendesk automation user ie support@zenoss.com or automation@zenoss.com
+# ChangeMe: should be zendesk automation user ie support@zenoss.com or automation@zenoss.com
 email_address = 'closs@zenoss.com'
-# change me to match above user
+
+# ChangeMe to match above user
 api_token = 'DeqYUrk7BCXBOE7V7Pehcw70oI5v37i58ZcDnoNh'
+
 # Use basic authentication
-#auth = HTTPBasicAuth(f'{email_address}/token', api_token)
 auth = HTTPBasicAuth('{}/token'.format(email_address), api_token)
 
 response = requests.request(
@@ -94,4 +113,3 @@ api_response = updateEvent(**data)
 # ToDo: convert to proper logging
 print(data)
 print(api_response)
-
